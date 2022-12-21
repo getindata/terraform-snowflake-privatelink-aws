c5933dbcab309a7de6ec0ef39e6c20d70fe3acc4
Dominik Gniewek-Węgrzyn <dominik.gniewek@getindata.com>
2022-12-21 14:53:08 +0100

    fix: name_from_descriptor generation issue

--- locals.tf
+++ locals.tf
@@ -3,9 +3,9 @@ locals {
 
   vpc_cidr_enabled = module.this.enabled && var.allow_vpc_cidr ? 1 : 0
 
-  name_from_descriptor = trim(replace(
+  name_from_descriptor = module.this.enabled ? trim(replace(
     lookup(module.this.descriptors, var.descriptor_name, module.this.id), "/${module.this.delimiter}${module.this.delimiter}+/", module.this.delimiter
-  ), module.this.delimiter)
+  ), module.this.delimiter) : null
 
   allowed_cidrs = concat(
     var.allow_vpc_cidr ? [one(data.aws_vpc.this).cidr_block] : [],
